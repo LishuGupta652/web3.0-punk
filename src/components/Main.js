4f200f3ebee477fa431a010eca7ad4cd52885930
lishu <lishugupta652@gmail.com>
2021-12-13 02:39:42 +0530

    collection card

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,12 +6,17 @@ const Main = () => {
       <div className="mainContent">
         <div className="punkHighLigh">
           <div className="punkContainer">
-            <img src="" alt="" />
+            <img
+              src="https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
+              alt=""
+            />
           </div>
         </div>
         <div className="punkDetails" style={{ color: "#fff" }}>
-          <div className="title">{activePunk.name}</div>
+          <div className="title">{"lishugupta"}</div>
+          <div className="itemNumber">{"#3"}</div>
         </div>
+        <div className="ownerDetails"></div>
       </div>
     </div>
   );
