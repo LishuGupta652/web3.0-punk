aea8e0bcfa9cc2b817dd122936cf57ab88d0a03f
lishu <lishugupta652@gmail.com>
2021-12-12 23:11:12 +0530

    collection card

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import weth from "../assets/images/weth.png";
 const CollectionCard = ({ id, name, traits, iamge }) => {
   return (
     <div className="collectionCard">
@@ -9,7 +9,7 @@ const CollectionCard = ({ id, name, traits, iamge }) => {
         {name} <div className="id"> . #{id}</div>
       </div>
       <div className="priceContainer">
-        <img src={weth} alt="" className="wethImage" />
+        <img src={weth} alt={name} className="wethImage" />
         <div className="price">{traits[0].value}</div>
       </div>
     </div>
