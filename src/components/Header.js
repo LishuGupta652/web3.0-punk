95a449a9ecc2883c12cebec3c27b5cd706207529
lishu <lishugupta652@gmail.com>
2021-12-12 22:41:49 +0530

    assets added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React from "react";
 import punk from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="logoContainer">
-        <img src={punk} alt="" />
+        <img src={punk} alt="punlog" className="punklogo" />
+      </div>
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="" />
+        </div>
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Collection, items or user"
+        />
+      </div>
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>MarketPlace</p>
+        <p>Create</p>
       </div>
     </div>
   );
