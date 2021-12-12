cca0f781e58bdab5ee9b8d9ce5c6a44ade7b3a19
lishu <lishugupta652@gmail.com>
2021-12-13 02:43:09 +0530

    collection card

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,7 +16,31 @@ const Main = () => {
           <div className="title">{"lishugupta"}</div>
           <div className="itemNumber">{"#3"}</div>
         </div>
-        <div className="ownerDetails"></div>
+        <div className="ownerDetails">
+          <div className="ownerImageContainer">
+            <img
+              src={
+                "https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
+              }
+              alt=""
+            />
+          </div>
+          <div className="ownerDetails">
+            <div className="ownerNameAndHandle">
+              <div>0x512C67ecE7670b9E192291e64912cAdd51B53802</div>
+              <div className="ownerHandle">@lishugupta652</div>
+            </div>
+            <div className="ownerLink">
+              <img src="" alt="" />
+            </div>
+            <div className="ownerLink">
+              <img src="" alt="" />
+            </div>
+            <div className="ownerLink">
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
