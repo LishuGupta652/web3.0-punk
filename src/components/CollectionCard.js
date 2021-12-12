046bfeac4251e5f5b123821ba4668601552c0992
lishu <lishugupta652@gmail.com>
2021-12-12 22:50:31 +0530

    header iamge

---
+++ src/components/CollectionCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CollectionCard = () => {
+  return (
+    <div className="collectionCard">
+      <h1>collection card</h1>
+    </div>
+  );
+};
+
+export default CollectionCard;
