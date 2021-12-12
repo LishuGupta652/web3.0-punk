0adf644879735998f5a1fed80d18ff710449053a
lishu <lishugupta652@gmail.com>
2021-12-12 23:20:07 +0530

    collection card

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -4,13 +4,14 @@ const CollectionCard = ({ id, name, traits, image }) => {
   return (
     <div className="collectionCard">
       <img src={image} alt="" />
-      <div className="details"></div>
-      <div className="name">
-        {name} <div className="id"> . #{id}</div>
-      </div>
-      <div className="priceContainer">
-        <img src={weth} alt={name} className="wethImage" />
-        <div className="price">{traits[0].value}</div>
+      <div className="details">
+        <div className="name">
+          {name} <div className="id"> . #{id}</div>
+        </div>
+        <div className="priceContainer">
+          <img src={weth} alt={name} className="wethImage" />
+          <div className="price">{traits[0].value}</div>
+        </div>
       </div>
     </div>
   );
