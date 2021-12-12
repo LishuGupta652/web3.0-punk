dae4ffb3419191e109c0314117c9d0d0413e3f51
lishu <lishugupta652@gmail.com>
2021-12-13 02:18:14 +0530

    collection card

--- src/components/PunkList.js
+++ src/components/PunkList.js
@@ -1,11 +1,18 @@
 import React from "react";
+import CollectionCard from "./CollectionCard";
 
 const PunkList = ({ punkListData }) => {
   return (
     <div>
       <div className="punkList">
         {punkListData.map((punk) => {
-          <div></div>;
+          <CollectionCard
+            key={punk.token.id}
+            id={punk.token.id}
+            name={punk.token_id}
+            traits={[{ value: 7 }]}
+            image="https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
+          />;
         })}
       </div>
     </div>
