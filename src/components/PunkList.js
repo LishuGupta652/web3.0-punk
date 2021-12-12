05289f2baf9089708f904eb38f03f35100d373a6
lishu <lishugupta652@gmail.com>
2021-12-13 02:18:51 +0530

    collection card

--- src/components/PunkList.js
+++ src/components/PunkList.js
@@ -9,9 +9,9 @@ const PunkList = ({ punkListData }) => {
           <CollectionCard
             key={punk.token.id}
             id={punk.token.id}
-            name={punk.token_id}
-            traits={[{ value: 7 }]}
-            image="https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
+            name={punk.name}
+            traits={punk.traits}
+            image={punk.image_original_url}
           />;
         })}
       </div>
