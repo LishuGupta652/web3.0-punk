266a4451a243da22d07045ae7fce57e71dcceb25
lishu <lishugupta652@gmail.com>
2021-12-12 22:59:03 +0530

    collection card

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const CollectionCard = () => {
+const CollectionCard = ({ id, name, traits, iamge }) => {
   return (
     <div className="collectionCard">
-      <h1>collection card</h1>
+      <img src={image} alt="" />
+      <div className="details"></div>
+      <div className="name">
+        {name} <div className="id"> . #{id}</div>
+      </div>
+      <div className="priceContainer">
+        <img src={weth} alt="" className="wethImage" />
+        <div className="price">{traits[0].value}</div>
+      </div>
     </div>
   );
 };
