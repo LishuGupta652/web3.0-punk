51bb66bdc8209bc654ce7e0e2c043804bacf30fe
lishu <lishugupta652@gmail.com>
2021-12-12 23:27:45 +0530

    fetching data from opensea

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const response = await axios.get(
+        "https://testnets.opensea.io/api/v1/assets?asset_contract_address='0x275eAb5D0a27a009EB43c6e9fD61080ba11e4944'"
+      );
+      setPunkListData(response.data);
+    };
+  }, []);
   return (
     <div className="App">
       <Header />
